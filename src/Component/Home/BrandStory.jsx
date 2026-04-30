586bdeefd562994ae73cdab39bca3f8841d06fce
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2026-04-30 08:53:08 +0400

    update BrandStory component

--- src/Component/Home/BrandStory.jsx
+++ src/Component/Home/BrandStory.jsx
@@ -1,21 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react'; 
 import BrandStoryVideo from '/assets/img/LEGENDARY_ORIGIN_EN_360.mp4';
 import { TbDiamondsFilled } from "react-icons/tb";
 import { Volume2, VolumeOff } from 'lucide-react';
-
+import { MdPause } from "react-icons/md";
+import { FaPlay } from "react-icons/fa";
 function BrandStory() {
     const [isMuted, setIsMuted] = useState(true)
+    const [isPaused, setIsPaused] = useState(false)
+           const videoRef = useRef(null);
     const toggleMute = () => {
        setIsMuted(!isMuted)
+    }
+   const togglePause = () => {
+       setIsPaused(!isPaused);
+       
+       if (videoRef.current) {
+           if (!isPaused) {
+               videoRef.current.pause();
+           } else {
+               videoRef.current.play();
+           }
+       }
     }
     return (
         <div className="max-w-[900px] mx-auto mb-[2rem]">
             <div className="bg-[#f5f5f5] text-center">
                 <h3 className='text-[28px] py-[1rem] font-optima'>Legendary Beauty For A Reason</h3>
                 <div className="relative w-fit m-auto ">
-        <video src={BrandStoryVideo} autoPlay muted={isMuted} loop playsInline className='' />
+      <video ref={videoRef} src={BrandStoryVideo} autoPlay muted={isMuted} loop playsInline className="" />
                 <div onClick={toggleMute} className=" absolute  p-2 bottom-4 right-4  flex items-center bg-[#ffffffe6] rounded-full h-fit">
                 {isMuted ? <VolumeOff size={14} /> : <Volume2 size={14} />}
+                </div>
+                <div onClick={togglePause} className=" absolute  p-2 bottom-4 right-13  flex items-center bg-[#ffffffe6] rounded-full h-fit">
+                {isPaused ? <FaPlay  size={14} /> : <MdPause size={14} />}
                 </div>
                 </div>
         
