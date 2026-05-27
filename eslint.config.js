16a4e3d121306207d5806e734ab73b9e299a36f3
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2026-05-27 07:31:09 +0400

    fix: resolve production build case-sensitivity and adjust eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,10 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn',
+      'react-refresh/only-export-components': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/rules-of-hooks': 'warn',
     },
   },
 ])
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './Component/Header'
 import AppRouter from './Router/AppRouter'
